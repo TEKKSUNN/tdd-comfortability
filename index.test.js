2da185c2857a2498b1374bfefcc5ad1207589ca9
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-18 16:27:05 +0800

    Make test cases for reverseString

--- index.test.js
+++ index.test.js
@@ -1,7 +1,11 @@
-import { capitalize } from "./index";
+import { capitalize, reverseString } from "./index";
 
 function testCapitalize(wordObject) {
   return expect(capitalize(wordObject.test)).toBe(wordObject.result);
+}
+
+function testReverseString(wordObject) {
+  return expect(reverseString(wordObject.test)).toBe(wordObject.result);
 }
 
 it('Capitalize works on lowercased words', () => {
@@ -29,4 +33,31 @@ it('Capitalize leaves already capitalized words alone', () => {
     {'test': 'Pizza', 'result': 'Pizza'},
   ];
   testCases.forEach(testCapitalize);
+});
+
+it('reverseString works on lowercased words', () => {
+  const testCases = [
+    {'test': 'abc', 'result': 'cba'},
+    {'test': 'good', 'result': 'doog'},
+    {'test': 'global', 'result': 'labolg'},
+  ];
+  testCases.forEach(testReverseString);
+});
+
+it('reverseString works on camel cased words', () => {
+  const testCases = [
+    {'test': 'camelCase', 'result': 'esaClemac'},
+    {'test': 'doNot', 'result': 'toNod'},
+    {'test': 'niceGame', 'result': 'emaGecin'},
+  ];
+  testCases.forEach(testReverseString);
+});
+
+it('reverseString works on reversed words', () => {
+  const testCases = [
+    {'test': 'tunoD', 'result': 'Donut'},
+    {'test': 'regrubmaH', 'result': 'Hamburger'},
+    {'test': 'azziP', 'result': 'Pizza'},
+  ];
+  testCases.forEach(testReverseString);
 });
