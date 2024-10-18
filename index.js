47eee0595a331ba144e8219646dcaadbebfdf2fc
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-18 17:43:38 +0800

    Make & export empty calculator object

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ export function capitalize(string) {
 export function reverseString(string) {
   return Array.from(string).reverse().join('');
 }
+
+export class calculator {
+  constructor() {}
+  static add(num1, num2) {}
+  static subtract(num1, num2) {}
+  static multiply(num1, num2) {}
+  static divide(num1, num2) {}
+}
