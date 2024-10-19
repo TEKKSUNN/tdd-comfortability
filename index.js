1705c763c85bee74223397102fbc12d2c81750bd
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-10-19 11:06:04 +0800

    Make calculate methods pass tests

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ export function reverseString(string) {
 
 export class calculator {
   constructor() {}
-  static add(num1, num2) {}
-  static subtract(num1, num2) {}
-  static multiply(num1, num2) {}
-  static divide(num1, num2) {}
+  static add(num1, num2) {
+    return num1 + num2;
+  }
+  static subtract(num1, num2) {
+    return num1 - num2;
+  }
+  static multiply(num1, num2) {
+    return num1 * num2;
+  }
+  static divide(num1, num2) {
+    return num1 / num2;
+  }
 }
